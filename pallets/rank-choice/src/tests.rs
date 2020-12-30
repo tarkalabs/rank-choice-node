f709817a9104f88a1d7b8a144d66d2eb4ab0e27e
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2020-12-30 10:43:51 -0400

    add first test for creating new poll

--- pallets/rank-choice/src/tests.rs
+++ pallets/rank-choice/src/tests.rs
@@ -5,19 +5,11 @@ use frame_support::{assert_ok, assert_noop};
 fn it_works_for_default_value() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(RankChoiceModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(RankChoiceModule::something(), Some(42));
-	});
-}
-
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			RankChoiceModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
-		);
+		let next_poll_id = RankChoiceModule::next_poll_id();
+		let content = br#"{"description":"test poll", "items": ["item1", "item2", "item3", "item4"]}"#.to_vec();
+		assert_ok!(RankChoiceModule::new_poll(Origin::signed(1), 4, content)); 
+		let poll = RankChoiceModule::poll_by_id(next_poll_id);
+		assert!(!&poll.is_none());
+		assert_eq!(poll.unwrap().num_items, 4);
 	});
 }
